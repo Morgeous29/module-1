807b2641dbd79a1ac2d0ac660b54b69b7ba20c44
Алексей <77805051+Morgeous29@users.noreply.github.com>
2024-10-14 22:51:40 +0300

    Add files via upload

---
+++ module_1_1.py
@@ -0,0 +1,9 @@
+name = 'Алексей'
+print('Name:', name)
+age = 27
+print('Age:', age)
+new_age = 2
+age = age + new_age
+print('New Age:', age)
+is_student = True
+print('Is Student:', is_student)
---
+++ module_1_2.py
@@ -0,0 +1,4 @@
+count_homework = 12
+hours = 1.5
+name_course = "Python"
+print('Курс: ' + name_course + ', всего задач:',str(count_homework) + ',', 'затрачено времени: ' + str(hours) + ',', 'среднее время выполнения', str(hours / count_homework) + '.')
